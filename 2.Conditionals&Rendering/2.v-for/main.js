42cb05c6c706015a6d6098d4a66821f515161226
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-05-28 13:46:12 +0100

    v-for

--- 2.Conditionals&Rendering/2.v-for/main.js
+++ 2.Conditionals&Rendering/2.v-for/main.js
@@ -2,6 +2,19 @@ new Vue({
   el: '#app',
   data: {
     show: false,
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      {
+        name: 'Ana',
+        age: 27,
+        color: 'pink',
+      },
+      {
+        name: 'Felipe',
+        age: 30,
+        color: 'black',
+      },
+    ],
   },
 
   computed: {
