a9f3394c041042b35c34a0f6787119138883bbb6
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-05-28 10:49:51 +0100

    styling dynamic

--- introduction/11.DynamicClass/main.js
+++ introduction/11.DynamicClass/main.js
@@ -2,11 +2,21 @@ new Vue({
   el: '#app',
   data: {
     attachRed: false,
+    color: 'green',
+    color2: 'blue',
+    color3: 'purple',
+    width: 100,
   },
 
   computed: {
     divClasses: function () {
       return { red: this.attachRed, blue: !this.attachRed };
+    },
+    styleFunction: function () {
+      return {
+        backgroundColor: this.color3,
+        width: this.width + 'px',
+      };
     },
   },
 });
